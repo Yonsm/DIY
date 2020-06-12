bf36ec0c16aeb7aad360f605b397263ea8cca2ff
Yonsm <Yonsm@qq.com>
2020-06-12 13:34:10 +0800

    Add Padavan restore script

---
+++ ROUTER/PADAVAN/setup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+cd "${0%/*}"
+
+# Router: Main Router (XY-C1)
+# Router2/Router3: WIFI AP (K2P)
+# ROUTER: Office Router (NEWIFI-MINI)
+# router: Friends' Router (NEWIFI3)
+if [ -z $1 ]; then HOST=Router; else HOST=$1; fi
+if [ -z $2 ]; then NAME=$HOST; else NAME=$2; fi
+
+[ -z $WING_HOST] && WING_HOST=192.168.1.9
+[ -z $WING_PORT] && WING_PORT=1080
+
+# SSH
+scp ../../../PrivY/SSH/authorized_keys $HOST:/etc/storage/ || echo "Usage: $0 [HOST] [NAME] [SSID]"
+ssh $HOST "chmod 600 /etc/storage/authorized_keys; [ ! -d ~/.ssh ] && mkdir ~/.ssh; cp /etc/storage/authorized_keys ~/.ssh"
+
+copy_files()
+{
+	for FILE in `ls -F $1`
+	do
+		if [ "${FILE: -1}" = "/" ]; then
+			ssh $HOST "[ ! -d /etc/$1/${FILE%?} ] && mkdir /etc/$1/${FILE%?}"
+			copy_files $1/${FILE%?}
+		else
+			[ "${FILE: -1}" = "*" ] && FILE=${FILE%?}
+			scp $1/$FILE $HOST:/etc/$1/$FILE
+		fi
+	done
+}
+
+# FILES
+echo '#!/bin/sh\nsync && echo 3 > /proc/sys/vm/drop_caches' > storage/started_script.sh
+if [ "$HOST" = "ROUTER" ] || [ "$HOST" = "Router" ] || [ "$HOST" = "router" ]; then
+	if [ "$HOST" = "Router" ]; then
+		echo "mdev -s" >> storage/started_script.sh
+	fi
+	if [ ! -z "$WING_HOST" ]; then
+		echo "wing $WING_HOST $WING_PORT $WING_PASS" >> storage/started_script.sh
+		echo '#!/bin/sh\nwing resume' > storage/post_iptables_script.sh
+	fi
+
+	# Contains https/dnsmasq/scripts in storage
+	copy_files storage
+elif [ "$HOST" = "Router2" ] || [ "$HOST" = "Router3" ]; then
+	#echo 'iwpriv ra0 set KickStaRssiLow=-85\niwpriv ra0 set AssocReqRssiThres=-80' >> storage/started_script.sh
+	echo 'iwpriv rai0 set KickStaRssiLow=-85\niwpriv rai0 set AssocReqRssiThres=-80' >> storage/started_script.sh
+	scp storage/started_script.sh $HOST:/etc/storage/
+fi
+
+# NVRAM
+scp nvram.sh $HOST:/tmp
+ssh $HOST "/tmp/nvram.sh $NAME $3"

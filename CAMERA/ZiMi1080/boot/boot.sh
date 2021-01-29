fd6b5dd12d2415b7a50d2af496496282597abe07
Yonsm <Yonsm@qq.com>
2021-01-30 00:37:13 +0800

    otafix

--- CAMERA/ZiMi1080/boot/boot.sh
+++ CAMERA/ZiMi1080/boot/boot.sh
@@ -7,13 +7,14 @@ sh /gm/config/vg_boot.sh /gm/config
 # Load DIY
 /mnt/media/mmcblk0p1/diy/diy.sh
 
-# Load DIY from wifi_start.sh on next boot
-grep /diy.sh /mnt/data/ot_wifi_tool/wifi_start.sh || echo /mnt/media/mmcblk0p1/diy/diy.sh >> /mnt/data/ot_wifi_tool/wifi_start.sh
-# Change from ft_boot to normal on next boot
-mv /mnt/media/mmcblk0p1/ft /mnt/media/mmcblk0p1/ft.bak
+# Load DIY from wifi_start.sh
+grep diy.sh /mnt/data/ot_wifi_tool/wifi_start.sh || echo /mnt/media/mmcblk0p1/diy/diy.sh >> /mnt/data/ot_wifi_tool/wifi_start.sh
 
 # Inject to OTA for DIY loading in new ROM
-grep inject.sh /mnt/data/miio_ota/post-ota.sh || sed -i '2 a/tmp/ld-uClibc.so.0 /tmp/busybox sh /mnt/media/mmcblk0p1/boot/inject.sh' /mnt/data/miio_ota/post-ota.sh
-exit
-cp /mnt/media/mmcblk0p1/boot/inject.sh /tmp/
-grep inject.sh /mnt/data/miio_ota/post-ota.sh || cp /mnt/media/mmcblk0p1/boot/post-ota.sh /mnt/data/miio_ota/
+grep otafix.sh /mnt/data/miio_ota/post-ota.sh || sed -i '2 a/tmp/ld-uClibc.so.0 /tmp/busybox sh /mnt/media/mmcblk0p1/boot/otafix.sh' /mnt/data/miio_ota/post-ota.sh
+#cp /mnt/media/mmcblk0p1/boot/otafix.sh /tmp/
+#grep otafix.sh /mnt/data/miio_ota/post-ota.sh || cp /mnt/media/mmcblk0p1/boot/post-ota.sh /mnt/data/miio_ota/
+
+# Change from ft_boot to normal on next boot
+mv /mnt/media/mmcblk0p1/ft /mnt/media/mmcblk0p1/ft.bak
+#reboot

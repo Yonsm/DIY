16edb08de3c72ae5508dd368e85b72d3179ac370
Yonsm <Yonsm@qq.com>
2021-01-29 23:42:09 +0800

    Refine comment

--- CAMERA/ZiMi1080/boot/boot.sh
+++ CAMERA/ZiMi1080/boot/boot.sh
@@ -4,15 +4,15 @@
 echo 0 > /tmp/ft_mode
 sh /gm/config/vg_boot.sh /gm/config
 
-# Load DIY script
+# Load DIY
 /mnt/media/mmcblk0p1/diy/diy.sh
 
-# Load diy from wifi_start.sh
+# Load DIY from wifi_start.sh on next boot
 grep /mnt/media/mmcblk0p1/diy/diy.sh /mnt/data/ot_wifi_tool/wifi_start.sh || echo /mnt/media/mmcblk0p1/diy/diy.sh >> /mnt/data/ot_wifi_tool/wifi_start.sh
 # Change from ft_boot to normal on next boot
 mv /mnt/media/mmcblk0p1/ft /mnt/media/mmcblk0p1/ft.bak
 
-# Inject in ROM update
+# Inject to OTA for DIY loading in new ROM
 if [ ! -f /mnt/data/miio_ota/inject.sh ]; then
   cp /mnt/media/mmcblk0p1/boot/inject.sh /mnt/data/miio_ota/
   cp /mnt/media/mmcblk0p1/boot/post-ota.sh /mnt/data/miio_ota/

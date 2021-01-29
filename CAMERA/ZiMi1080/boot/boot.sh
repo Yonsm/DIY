08d8843dcbe7e3cfa4dbfb6f0728b1d59b30a6c4
Yonsm <Yonsm@qq.com>
2021-01-29 23:37:34 +0800

    Refine boot

--- CAMERA/ZiMi1080/boot/boot.sh
+++ CAMERA/ZiMi1080/boot/boot.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-# Restore to normal mode
+# Fake to normal mode
 echo 0 > /tmp/ft_mode
 sh /gm/config/vg_boot.sh /gm/config
 
 # Load DIY script
 /mnt/media/mmcblk0p1/diy/diy.sh
 
-# Inject in ROM update
-cp /mnt/media/mmcblk0p1/boot/post-ota.sh /mnt/data/miio_ota/post-ota.sh
-cp /mnt/media/mmcblk0p1/boot/inject.sh /tmp/inject.sh
-exit
+# Load diy from wifi_start.sh
+grep /mnt/media/mmcblk0p1/diy/diy.sh /mnt/data/ot_wifi_tool/wifi_start.sh || echo /mnt/media/mmcblk0p1/diy/diy.sh >> /mnt/data/ot_wifi_tool/wifi_start.sh
+# Change from ft_boot to normal on next boot
+mv /mnt/media/mmcblk0p1/ft /mnt/media/mmcblk0p1/ft.bak
 
-# TODO: Alt injection method not work?
+# Inject in ROM update
 if [ ! -f /mnt/data/miio_ota/inject.sh ]; then
-  cp /mnt/media/mmcblk0p1/boot/inject.sh /mnt/data/miio_ota/inject.sh
-  sed -i '2 a/tmp/ld-uClibc.so.0 /tmp/busybox sh /mnt/data/miio_ota/inject.sh' /mnt/data/miio_ota/post-ota.sh
+  cp /mnt/media/mmcblk0p1/boot/inject.sh /mnt/data/miio_ota/
+  cp /mnt/media/mmcblk0p1/boot/post-ota.sh /mnt/data/miio_ota/
+  #sed -i '2 a/tmp/ld-uClibc.so.0 /tmp/busybox sh /mnt/data/miio_ota/inject.sh' /mnt/data/miio_ota/post-ota.sh
 fi
--- CAMERA/diy/diy.sh
+++ CAMERA/diy/diy.sh
@@ -14,5 +14,4 @@ fi
 [ -f busybox ] && ./busybox telnetd
 [ -f lighttpd ] && ./lighttpd -f lighttpd.conf
 [ -f rtspd ] && ./rtspd -s &
-[ -f rtspsvr ] && ./rtspsvr &
 [ -f rtsp_basic ] && ./rtsp_basic &

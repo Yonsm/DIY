2ecd3e5307d44b60e5d69005b44a6404d50e73af
Yonsm <Yonsm@qq.com>
2022-10-12 19:38:03 +0800

    Refine setup.sh; Add auto ssh

--- ROUTER/AX6000/setup.sh
+++ ROUTER/AX6000/setup.sh
@@ -3,12 +3,17 @@
 [ $# != 2 ] && echo "Usage: $0 [HOST] [URL] " && exit
 
 cd `dirname $0`
-ssh root@$1 "mkdir /data/wing"
+
+echo "Execute command as below:"
+echo "ssh root@$1 'mkdir /data/wing'"
 for FILE in `ls|tr " " "?"`
 do
 	FILE=`echo $FILE|tr "?" " "`
-	if [ $FILE != setup.sh ]; then
-		echo scp $FILE root@$1:/data/wing/
+	if [ ${FILE##*.} != sh ]; then
+		echo "scp $FILE root@$1:/data/wing/"
 	fi
 done
-ssh root@$1 "/data/wing/wing -i $2"
+echo "ssh root@$1 '/data/wing/wing -i $2'"
+
+# SSH: https://www.right.com.cn/forum/thread-8253125-1-1.html
+# WEB@Any: vi /etc/nginx/miwifi-webinitrd.conf; Insert 'set $isluci “1”;' before 'set $finalvar $canproxy $isluci'

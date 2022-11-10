8bf02de091252e4aa0e5ff154cee12575d8cfa64
Yonsm <Yonsm@qq.com>
2022-11-10 16:36:45 +0800

    Entware for DSM

--- DSM/Setup.sh
+++ DSM/Setup.sh
@@ -1,3 +1,38 @@
+#!/bin/sh
+
+#
+sudo -i
+
+# Entware
+mkdir /opt
+chmod 777 /opt
+wget -O - https://bin.entware.net/armv7sf-k3.2/installer/generic.sh | /bin/sh
+echo ". /opt/etc/profile" >> /etc/profile
+# Add Task on bootup: /opt/etc/init.d/rc.unslung start
+
+# IPTV
+opkg install udpxy
+
+# Misc
+opkg install iperf3
+
+# Python3
+opkg install python3 python3-pip python3-dev
+
+#
+export HOME=/opt/pip
+export TMPDIR=/opt/tmp
+#--no-cache-dir
+wget -O - http://bin.entware.net/mipselsf-k3.4/installer/generic.sh | /bin/sh
+opkg install python3 python3-pip
+opkg install gcc python3-dev
+
+pip install homeasssistant
+
+
+
+
+
 
 #Aria Deamon
 ln -s /volume1/Downloads/.opt /opt

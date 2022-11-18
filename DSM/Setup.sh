d039fa68d18914353bb1a2103b52174d9e2a9200
Yonsm <Yonsm@qq.com>
2022-11-18 10:30:29 +0800

    DSM

--- DSM/Setup.sh
+++ DSM/Setup.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
-#
-sudo -i
+# Docker: https://gathering.tweakers.net/forum/list_messages/2113116, 19.03.8 is the lastest good version
+curl "https://download.docker.com/linux/static/stable/armhf/docker-19.03.8.tgz" | tar -xz -C /usr/bin --strip-components=1
+mkdir /etc/docker
+cat << \EOF > /etc/docker/daemon.json
+{
+  "storage-driver": "vfs",
+  "iptables": false,
+  "data-root": "/volume1/@docker"
+}
+EOF
+cat << \EOF > /etc/systemd/system/docker.service
+[Unit]
+Description=Service Docker
+
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl starts docker
+systemctl status docker
+modprobe iptable-nat
+reboot
+
+docker run hello-world
+
+# HomeAssistant: https://www.home-assistant.io/installation/linux#install-home-assistant-container
+docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=Asia/Shanghai -v /volume1/@docker/.homeassistant:/config --network=host ghcr.io/home-assistant/home-assistant:stable
+docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock --restart=always --name portainer portainer/portainer-ce
 
 # Entware
 mkdir /opt
@@ -14,6 +45,11 @@ echo ". /opt/etc/profile" >> /etc/profile
 opkg install busybox
 busybox passwd admin
 busybox passwd root
+
+# Network
+opkg install iperf3
+cd /opt/bin; wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest; ./speedtest
+exit
 
 # IPTV
 # USB NET: https://www.31du.cn/software/dsm7-2-5g-rtl815x.html
@@ -30,14 +66,18 @@ PATH=/opt/sbin:/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:
 . /opt/etc/init.d/rc.func
 EOF
 
+# Mosquitto
+opkg install mosquitto-nossl
+cat << \EOF > /opt/etc/mosquitto/mosquitto.conf
+allow_zero_length_clientid true
+listener 1883
+allow_anonymous true
+EOF
+
+
 # Python3
 opkg install python3 python3-pip
 python3 -m pip install --upgrade pip setuptools
-
-# Misc
-opkg install iperf3
-cd /opt/bin; wget -O speedtest https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest; ./speedtest
-exit
 
 # ======== Home Assistant: ERROR on Rust ========
 opkg gcc python3-dev
@@ -59,11 +99,3 @@ d /opt/lib
 ln -s libffi.so.8 libffi.so
 
 pip3 install homeassistant
-
-# Mosquitto
-opkg install mosquitto-nossl
-cat << \EOF > /opt/etc/mosquitto/mosquitto.conf
-allow_zero_length_clientid true
-listener 1883
-allow_anonymous true
-EOF

d0f4405de7fec5a99770882c79632cc87e12035d
Yonsm <Yonsm@qq.com>
2022-12-03 00:17:13 +0800

    Cleanup

--- OSX/VNC/DDVNC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd "${0%/*}"
-CDIR=$(cd "${0%/*}"; pwd)
-
-curl -s -u xxxx:xxxx http://members.3322.net/dyndns/update?hostname=xxxx.f3322.net
-"$CDIR/UPNPC" -a `ifconfig | grep 'inet.*netmask.*broadcast' | awk '{print $2}'` 5900 5900 TCP
-open "$CDIR/VNC.png"
--- ROUTER/RM2100/redmi/showiface.py
+++
@@ -1,4 +0,0 @@
-from scapy.all import *
-
-#show_interfaces()
-print(show_interfaces())

4179a829a731d7006a5e1139b2a48ceb7bcc8654
Yonsm <Yonsm@qq.com>
2023-01-11 01:30:33 +0800

    LibreElec

--- SERVER/LibreElec.sh
+++ SERVER/LibreElec.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # Bridge
 ip link add name br0 type bridge
 ip link set br0 up
@@ -10,3 +11,10 @@ route add -net default gw 192.168.1.1
 ip link set eth1 master br0
 ip link set eth2 master br0
 ip link set eth3 master br0
+
+# Entware
+# https://cloud.tencent.com/developer/article/1831933
+# https://www.linuxprobe.com/install-libreelec-entware.html
+mkdir -p /storage/opt
+mount -o bind /storage/opt /opt
+wget -O - https://bin.entware.net/x64-k3.2//installer/generic.sh | sh

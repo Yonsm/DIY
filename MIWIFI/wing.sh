b6d0afd06e4ecaadb8bf605975f7e6ba61008207
Yonsm <Yonsm@qq.com>
2025-06-29 10:44:14 +0700

    chmod for wing

--- MIWIFI/wing.sh
+++ MIWIFI/wing.sh
@@ -16,16 +16,19 @@ if [ ! -f $_DST/wing ]; then
 	for FILE in dns-forwarder gfwlist.conf ipt2socks llibsodium.so.23 ibudns.so.0 ss-redir ssr-redir trojan wing; do
 		curl -o  $_DST/$FILE $_SRC/$FILE
 	done
+	chmod +x /data/wing/*
 fi
 
 if [ ! -z "$1" ]; then
 	if [ -f /etc/config/miwifi ]; then
+		_CMD="$_DST/wing restart $1"
 		uci set firewall.wing=include
 		uci set firewall.wing.type="script"
-		uci set firewall.wing.path="$_DST/wing restart $1"
+		uci set firewall.wing.path="$_CMD"
 		uci set firewall.wing.enabled="1"
 		uci commit firewall
 		uci show firewall.wing
+		$_CMD
 	else
 		echo "Not install with $1"
 		exit 1
